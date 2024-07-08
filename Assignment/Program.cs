991957fd6de43b196dbbe1dd7a3fcf6050f84cb6
Gamgom29 <abdogamgom2015@gmail.com>
2024-07-08 03:57:59 +0300

    Q02

--- Assignment/Program.cs
+++ Assignment/Program.cs
@@ -10,16 +10,22 @@
         Saturday = 6 ,
         Sunday = 7 ,
     }
+
+
     internal class Program
     {
         static void Main(string[] args)
         {
             #region Q01
-           /* Console.WriteLine("Week Days :");
-            for (int i = 1; i <= 7; i++)
-            {
-                Console.WriteLine((WeekDays) i);
-            }*/
+            /* Console.WriteLine("Week Days :");
+             for (int i = 1; i <= 7; i++)
+             {
+                 Console.WriteLine((WeekDays) i);
+             }*/
+            #endregion
+
+            #region Q02
+
             #endregion
         }
     }
