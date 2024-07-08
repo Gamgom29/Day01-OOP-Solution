be9187650efac8865754ccf153afde3f522302fc
Gamgom29 <abdogamgom2015@gmail.com>
2024-07-08 03:37:50 +0300

    Q01

---
+++ Assignment/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Assignment
+{
+    enum WeekDays
+    {
+        Monday = 1 , 
+        Tuesday = 2,
+        Wednesday = 3 ,
+        Thursday = 4 ,
+        Friday = 5 ,
+        Saturday = 6 ,
+        Sunday = 7 ,
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Q01
+           /* Console.WriteLine("Week Days :");
+            for (int i = 1; i <= 7; i++)
+            {
+                Console.WriteLine((WeekDays) i);
+            }*/
+            #endregion
+        }
+    }
+}
